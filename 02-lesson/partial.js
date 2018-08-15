44f6a9d15fa06dae451c6a40896847a2c0e87400
Alan Shaw <alan@nodename.com>
2018-08-15 17:42:54 -0400

    Updated code

--- 02-lesson/partial.js
+++ 02-lesson/partial.js
@@ -1,21 +1,17 @@
 const plus = (x, y) => x + y;
 const lessThan = (x, y) => x < y;
 
-// specializing by using a fixed first argument:
-
 const plus1 = y => plus(1, y);
 const twoIsLessThan = y => lessThan(2, y);
 
 const makePlusX = x => y => plus(x, y);
 /* old style:
-
 function makePlusX(x) {
   return function(y) {
     return plus(x, y);
   };
 }
 */
-
 const plus2 = makePlusX(2);
 console.log(plus2(5));
 console.log(makePlusX(2)(5));
